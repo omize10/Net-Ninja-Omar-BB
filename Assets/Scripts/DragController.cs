e6629fb99b066c0079da4a8fdb4ab268bb5b06e9
genporterfield <87214684+omize10@users.noreply.github.com>
2023-11-27 18:29:46 -0800

    Working off video 2:28 working with DargCloider
    Link https://www.youtube.com/watch?time_continue=148&v=kVH5reaAyII&embeds_referring_euri=https%3A%2F%2Fwww.bing.com%2F&embeds_referring_origin=https%3A%2F%2Fwww.bing.com&source_ve_path=MTI3Mjk5LDEzOTExNywxMzkxMTcsMTM5MTE3LDI4NjYzLDI4NjYzLDI4NjY2&feature=emb_logo

---
+++ Assets/Scripts/DragController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragController : MonoBehaviour
+{
+
+    public LineRenderer line;
+    public Rigidbody2D rb;
+
+}
