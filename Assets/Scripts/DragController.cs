cef9823c1d7b411897cd5a93e6e3bfdb4037f167
sanaaskari <137968646+TomWestCodeNinja@users.noreply.github.com>
2023-12-04 18:33:44 -0800

    Done With Video Working to making the ball go throu thee hoop with colder need to add ssytem to make point and points go up

--- Assets/Scripts/DragController.cs
+++ Assets/Scripts/DragController.cs
@@ -8,6 +8,91 @@ public class DragController : MonoBehaviour
     public LineRenderer line;
     public Rigidbody2D rb;
 
-    public float 
+    public float dragLimit = 3f;
+    public float forceToAdd = 10f;
 
+    private Camera cam;
+    private bool isDragging;
+
+    Vector3 MousePosition
+    {
+        get
+        {
+            Vector3 pos = cam.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0f;
+            return pos;
+        }
+    }
+
+
+    private void Start()
+    {
+        cam = Camera.main;
+        line.positionCount = 2;
+        line.SetPosition(0, Vector2.zero);
+        line.SetPosition(1, Vector2.zero);
+        line.enabled = false;
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !isDragging)
+        {
+            DragStart();
+        }
+
+        if (isDragging)
+        {
+            Drag();
+        }
+
+        if(Input.GetMouseButtonUp(0) && isDragging)
+        {
+            DragEnd();
+        }
+    }
+
+    void DragStart()
+    {
+        line.enabled = true;
+        isDragging = true;
+        line.SetPosition(0, MousePosition);
+    }
+
+    void Drag()
+    {
+        Vector3 startpos = line.GetPosition(0);
+        Vector3 currentPos = MousePosition;
+
+        Vector3 distance = currentPos - startpos;
+
+        
+
+        if (distance.magnitude <= dragLimit)
+        {
+            line.SetPosition(1, currentPos);
+        }
+        else
+        {
+            Vector3 limitVector = startpos + (distance.normalized * dragLimit);
+            line.SetPosition(1, limitVector);
+        }
+    }
+    
+    private void DragEnd()
+    {
+        isDragging = false;
+        line.enabled = false;
+
+        Vector3 startpos = line.GetPosition(0);
+        Vector3 currentPos = line.GetPosition(1);
+
+        Vector3 distance = currentPos - startpos;
+        Vector3 finalForce = distance * forceToAdd;
+
+
+        rb.AddForce(-finalForce, ForceMode2D.Impulse);
+    }
 }
+
