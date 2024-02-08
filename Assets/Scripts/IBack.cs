74f84040d694f67f8811089232d5cdf3c41053db
codeninjas <129804231+EthanMel@users.noreply.github.com>
2024-02-07 18:28:36 -0800

    Done prottype

---
+++ Assets/Scripts/IBack.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IBack : MonoBehaviour
+{
+    // This method is called when the replay button is clicked
+    public void ReplayGame()
+    {
+        // Load the main scene (assuming it's at build index 0)
+        SceneManager.LoadScene(2);
+    }
+}
