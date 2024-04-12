1e8d7cd228b33d78d9177bfab207cb118b9df8a8
sanaaskari <124409893+datiscodeninjas@users.noreply.github.com>
2024-04-11 19:32:22 -0700

    Working on Ball neting new one every time reach new highscore background sence not working beeacuse it is not showing  buttons as part of arry not showing and arry buttons not showing in inpcotor

---
+++ Assets/Scripts/BallSeter.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSeter : MonoBehaviour
+{
+    public GameObject[] buttons;
+    public HoopManger2 HM2;
+    public int amountOfBallsUnlocked = 0;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    void Start()
+    {
+        HM2 = GameObject.Find("ball").GetComponent<HoopManger2>();
+        buttons = GameObject.FindGameObjectsWithTag("EquipArry");
+        amountOfBallsUnlocked += 1;
+    }
+
+
+    void Update()
+    {
+        if (HM2.hasHappened == true)
+        {
+            GameObject currentButton = buttons[amountOfBallsUnlocked];
+            currentButton.SetActive(true);
+            HM2.hasHappened = false;
+        }
+    }
+
+
+}
--- Assets/Scripts/HoopManger2.cs
+++ Assets/Scripts/HoopManger2.cs
@@ -6,20 +6,30 @@ public class HoopManger2 : MonoBehaviour
 { // ******* every time you beat your highscore you unlcok new backgound *******!!!!!!!!!!!!!!
     public Text scoreText;
     public GameObject IMG;
+
     public int hoopsRequired = 3; 
     //public GameObject hoop;
     private int currentScore = 0;
     private int previousHighScore;
+
+    public bool hasHappened;
     
     void wait()
     {
         IMG.SetActive(false);
     }
 
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     private void Start()
     {
         //PlayerPrefs.SetInt("HighScore", 0);//DELETE THIS LINE AFTER RESETTING HIGH SCORE
         previousHighScore = PlayerPrefs.GetInt("HighScore", 0);
+
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -46,8 +56,14 @@ public class HoopManger2 : MonoBehaviour
                     PlayerPrefs.Save();
 
                     IMG.SetActive(true);
-                    Invoke("wait", 6f);
+                    Invoke("wait", 100f);
+
+
+                    hasHappened = true; 
+
+
                     
+
                 }
                 SceneManager.LoadScene("LevelUp#1");
             }
