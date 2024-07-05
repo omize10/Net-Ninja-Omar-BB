a220073ce5224e0c529215e6f149f165e22ff0a9
omize10 <87214684+omize10@users.noreply.github.com>
2024-07-04 17:33:31 -0700

    1
    Bulit out sytem but geting erro in Chckpoin system sence and level one will not load need to fix

--- Assets/Scenes/Scripts/LoaderSytemManger.cs
+++ Assets/Scenes/Scripts/LoaderSytemManger.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class LoaderSytemManger : MonoBehaviour
 {
     public HoopManger2 HM2;
+    public GameObject Lten;
+    public GameObject LFifteen;
+    public GameObject Ltwemty;
 
     void Start()
     {
@@ -18,32 +21,39 @@ public class LoaderSytemManger : MonoBehaviour
         
     }
 
-    void LevelTen()
+     public void LevelTen()
     {
         int currentHighScore = PlayerPrefs.GetInt("HighScore", 0);
         if (currentHighScore >= 11 && currentHighScore < 16)
         {
+            Lten.SetActive(true);
             SceneManager.LoadScene("Level10");
         }
     }
 
-    void LevelFifteen()
+    public void LevelFifteen()
     {
 
         int currentHighScore = PlayerPrefs.GetInt("HighScore", 0);
         if (currentHighScore >= 16 && currentHighScore < 21)
         {
+            LFifteen.SetActive(true);
             SceneManager.LoadScene("Level15");
         }
 
     }
-        void LevelTwenty()
+        public void LevelTwenty()
         {
             int currentHighScore = PlayerPrefs.GetInt("HighScore", 0);
             if (currentHighScore >= 21 && currentHighScore < 25)
             {
+                Ltwemty.SetActive(true);
                 SceneManager.LoadScene("Level20");
             }
+        }
+
+        public void Levelone(){
+            SceneManager.LoadScene("Level1");
         }
 
     }
